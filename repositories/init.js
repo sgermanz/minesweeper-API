5e56db38c037d35dbd8114bfd928322728eb66a8
German Svriz <germans@exo.com.ar>
2020-05-24 15:29:29 -0300

    Updated mongodb connection

--- repositories/init.js
+++ repositories/init.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 function initialize(){
-    let mongoPath = process.env.MONGO_PATH || "localhost";
-    let mongoPort = process.env.MONGO_PORT || "27017";
-    let mongoDB = process.env.MONGO_DB || "default";
-    let connection = 'mongodb://' + mongoPath +':'+ mongoPort +'/' + mongoDB;
+    let connection = process.env.MONGO_URI;
     mongoose.connect(connection);
 }
 
